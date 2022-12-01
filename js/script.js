c45db9b1a27f3ce08698298f0564bdfcada011f8
stacy_r <stacyrileyca@gmail.com>
2022-12-01 15:02:19 -0500

    Added event listener to remove transition property and added finishing styles

--- js/script.js
+++ js/script.js
@@ -5,10 +5,30 @@
     //Here we are selecting the class of key to make it visually change:
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     // console.log(audio);
-    if(!audio) return; //Stops function from running if non-data-key is pressed:
-    audio.currentTime = 0; //Rewind to the start if pressed over and over again:
+    
+    //Stops function from running if non-data-key is pressed:
+    if(!audio) return;
+    
+    //Rewind to the start if pressed over and over again:
+    audio.currentTime = 0; 
     audio.play();
 
     key.classList.add("playing");
-  })
+  });
 
+  //Function removes the transition property from the key:
+ function removeTransition(e){
+    //Skip it if it doesn't have the propertyName of transform:
+    if(e.propertyName !== 'transform') return;
+    // console.log(e.propertyName)
+
+    //This is referring to key:
+    this.classList.remove('playing');
+ }
+ 
+ //Function to run after Transition end event in class of .keys is over:
+ const keys = document.querySelectorAll(".key");
+ //Here we are looping through the array and adding this addEventListener to every key in the array:
+ keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+ 
+ 
